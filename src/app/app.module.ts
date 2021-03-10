cbf5c059b5f98d8c4b580ec2ff0312a65f8c9a7f
AK <hello@hello.com>
2021-03-10 08:44:01 +0100

    Initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ChartComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
